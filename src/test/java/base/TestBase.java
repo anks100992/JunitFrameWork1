ccb4f23d2789456700d1a26823d2c04814381f9d
anks100992 <arpitnigam100992@gmail.com>
2020-12-17 17:31:07 +0530

    modified the TestBase class in which added the method for getting the browser name from cmd if no browser send then the default will be send as chrome

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -3,6 +3,7 @@ package base;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,19 +12,35 @@ public class TestBase
 {
 	protected WebDriver driver;
 	protected String base_url = "https://amazon.in/";
-	protected int implicit_wait_timeout_in_sec = 20;
-
+	
+	 private String getBrowserName(){
+	        String browserDefault = "chrome"; //Set by default
+	        String browserSentFromCmd = System.getProperty("browser");
+	        //mvn clean install -Dbrowser=safari
+	        //browserSentFromCmd = safari
+	        if (browserSentFromCmd==null){
+	            return browserDefault;
+	        }else{
+	            return browserSentFromCmd;
+	        }
+	    }
+	
 	@Before
 	public void setup()
 	{
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(implicit_wait_timeout_in_sec, TimeUnit.SECONDS);
+		String browser = getBrowserName();
+			
+		try{
+            //Use WebDriver Factory to get the Driver object instead of driver = new ChromeDriver()
+            driver = WebDriverFactory.getWebDriverForBrowser("chrome");
+        }catch(Exception e){
+            e.printStackTrace();
+            Assert.fail("Incorrect Browser Sent. Check the Stack Trace");
+        }
 	}
 
 	@After
-	public void Clean_UP() throws InterruptedException
+	public void Clean_UP() throws InterruptedException 
 	{
 		Thread.sleep(5000);	
 		driver.quit();
